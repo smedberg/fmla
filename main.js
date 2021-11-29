94ab32f58f39afefa8ba3a09258536cd57de7aba
smedberg <>
2021-11-29 20:29:52 +0000

    Deploying to main from @ smedberg/smedberg.github.io@6111b267a472d277aa44c6ec66f3476b53fb7119 🚀

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ import { EligibilitiesDisplay } from './eligibilities_display.js';
 var eligibilitiesDisplay = void 0;
 
 function onSurveyComplete(results) {
+	console.log("Survey is complete:", eligibilityMatch);
 	var eligibilityMatch = getEligibilityMatch(results.valuesHash);
 	if (eligibilityMatch !== null) {
+		console.log('In onSurveyComplete, found match ', eligibilityMatch);
 		eligibilitiesDisplay.handleChange(eligibilityMatch.eligibilities);
 	} else {
+		console.log('In onSurveyComplete, did not find eligibility match ');
 		// No matches- display warning
 	}
-
-	console.log("Survey is complete:", eligibilityMatch);
 }
 
 ReactDOM.render(React.createElement(Survey.Survey, { json: surveyJSON, onComplete: onSurveyComplete }), document.getElementById("surveyContainer"));
